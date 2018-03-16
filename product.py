cdb4a95a0778dadb249e08db92382ff2d45a1f04
Laboratorio de Software <lds@unac.edu.co>
2018-03-15 20:14:51 -0500

    first commit

---
+++ product.py
@@ -0,0 +1,14 @@
+from flask_restful import Resource
+
+class Product(Resource):
+    def get(self):
+        return {
+            'products': ['Ice Cream',
+                        'Chocolate',
+                        'Morcilla',
+                        'Fruit',
+                        'Pañales',
+                        'Leche',
+                        'Hola Mundo',
+                        'Anthony']
+        }
